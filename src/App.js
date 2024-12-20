788d9190d12564eb51c84c949f9503aa1ed9aab6
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2024-12-19 23:44:11 -0700

    starter code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Hello from "./components/Hello";
+
+function App() {
+
+	return (
+		<div>
+			<div>
+				<Header></Header>
+			</div>
+			<div>
+				<Hello></Hello>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Hello/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+	return (
+	<main>
+		<h2>Hello World!</h2>
+	</main>
+	);
+}
+
+export default About;
